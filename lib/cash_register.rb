6f0722343be31fc5cbb4ae9ba9837f2510348cd2
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-17 04:37:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,13 @@
 class CashRegister
-  @items = []
+
 
   attr_accessor :total, :discount
+  attr_reader :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(title, price, quantity = 1)
@@ -18,9 +20,5 @@ class CashRegister
     end
     self.total -= (@discount/100.0)*self.total
     "After the discount, the total comes to $#{@total.to_i}."
-  end
-
-  def items
-
   end
 end

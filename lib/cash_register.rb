009733122a1f256f980a16f22bafd7ec689e1bc4
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-17 04:32:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,6 @@ class CashRegister
 
   def apply_discount
     self.total -= (@discount/100.0)*self.total
-    "After the discount, the total comes to $#{@total}."
+    "After the discount, the total comes to $#{@total.to_i}."
   end
 end
